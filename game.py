d2e8cfce628acc5b6c763bc7cd15647cbf5f0a42
prady001 <gabriel.prady1@gmail.com>
2023-10-26 17:06:07 -0300

    bridges

--- game.py
+++ game.py
@@ -1,208 +1,71 @@
-import random
-import os
-
 import pygame
+from random import randint
 
-os.environ['SDL_VIDEO_CENTERED'] = '1' # call before pygame.init()
-pygame.init()
-info = pygame.display.Info()
-screen_width, screen_height = info.current_w, info.current_h
-window_width, window_height = screen_width - 800, screen_height - 150
+dimensoes = (1680, 1050)  # Define as dimensões da janela do jogo
 
-timer = pygame.time.Clock()
-fps = 60
-
-pygame.display.set_caption('Classic Donkey Kong Rebuild!')
-# pygame.display.set_icon('image file')
-
-screen = pygame.display.set_mode([window_width, window_height])
-section_width = window_width // 32
-section_height = window_height // 32
-slope = section_height // 8
-
-barrel_spawn_time = 360
-barrel_count = barrel_spawn_time / 2
-barrel_time = 360
-barrel_img = pygame.transform.scale(pygame.image.load('assets/images/barrels/barrel.png'), (section_width * 1.5, section_height * 2))
-
-start_y = window_height - 2 * section_height
-row2_y = start_y - 4 * section_height
-row3_y = row2_y - 7 * slope - 3 * section_height
-row4_y = row3_y - 4 * section_height
-row5_y = row4_y - 7 * slope - 3 * section_height
-row6_y = row5_y - 4 * section_height
-row6_top = row6_y - 4 * slope
-row5_top = row5_y - 8 * slope
-row4_top = row4_y - 8 * slope
-row3_top = row3_y - 8 * slope
-row2_top = row2_y - 8 * slope
-row1_top = start_y - 5 * slope
-active_level = 0
-levels = [{'bridges': [(1, start_y, 15), (16, start_y - slope, 3),
-                       (19, start_y - 2 * slope, 3), (22, start_y - 3 * slope, 3),
-                       (25, start_y - 4 * slope, 3), (28, start_y - 5 * slope, 3),
-                       (25, row2_y, 3), (22, row2_y - slope, 3),
-                       (19, row2_y - 2 * slope, 3), (16, row2_y - 3 * slope, 3),
-                       (13, row2_y - 4 * slope, 3), (10, row2_y - 5 * slope, 3),
-                       (7, row2_y - 6 * slope, 3), (4, row2_y - 7 * slope, 3),
-                       (2, row2_y - 8 * slope, 2), (4, row3_y, 3),
-                       (7, row3_y - slope, 3), (10, row3_y - 2 * slope, 3),
-                       (13, row3_y - 3 * slope, 3), (16, row3_y - 4 * slope, 3),
-                       (19, row3_y - 5 * slope, 3), (22, row3_y - 6 * slope, 3),
-                       (25, row3_y - 7 * slope, 3), (28, row3_y - 8 * slope, 2),
-                       (25, row4_y, 3), (22, row4_y - slope, 3),
-                       (19, row4_y - 2 * slope, 3), (16, row4_y - 3 * slope, 3),
-                       (13, row4_y - 4 * slope, 3), (10, row4_y - 5 * slope, 3),
-                       (7, row4_y - 6 * slope, 3), (4, row4_y - 7 * slope, 3),
-                       (2, row4_y - 8 * slope, 2), (4, row5_y, 3),
-                       (7, row5_y - slope, 3), (10, row5_y - 2 * slope, 3),
-                       (13, row5_y - 3 * slope, 3), (16, row5_y - 4 * slope, 3),
-                       (19, row5_y - 5 * slope, 3), (22, row5_y - 6 * slope, 3),
-                       (25, row5_y - 7 * slope, 3), (28, row5_y - 8 * slope, 2),
-                       (25, row6_y, 3), (22, row6_y - slope, 3),
-                       (19, row6_y - 2 * slope, 3), (16, row6_y - 3 * slope, 3),
-                       (2, row6_y - 4 * slope, 14), (13, row6_y - 4 * section_height, 6),
-                       (10, row6_y - 3 * section_height, 3)],
-           'ladders': [(12, row2_y + 6 * slope, 2), (12, row2_y + 26 * slope, 2),
-                       (25, row2_y + 11 * slope, 4), (6, row3_y + 11 * slope, 3),
-                       (14, row3_y + 8 * slope, 4), (10, row4_y + 6 * slope, 1),
-                       (10, row4_y + 24 * slope, 2), (16, row4_y + 6 * slope, 5),
-                       (25, row4_y + 9 * slope, 4), (6, row5_y + 11 * slope, 3),
-                       (11, row5_y + 8 * slope, 4), (23, row5_y + 4 * slope, 1),
-                       (23, row5_y + 24 * slope, 2), (25, row6_y + 9 * slope, 4),
-                       (13, row6_y + 5 * slope, 2), (13, row6_y + 25 * slope, 2),
-                       (18, row6_y - 27 * slope, 4), (12, row6_y - 17 * slope, 2),
-                       (10, row6_y - 17 * slope, 2), (12, -5, 13), (10, -5, 13)],
-          'hammers': [(4, row6_top + section_height), (4, row4_top+section_height)],
-           'target': (13, row6_y - 4 * section_height, 3)}]
-
-
-class Barrel(pygame.sprite.Sprite):
-    def __init__(self, x_pos, y_pos):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.rect = self.image.get_rect()
-        self.rect.center = (x_pos, y_pos)
-        self.y_change = 0
-        self.x_change = 1
-        self.pos = 0
-        self.count = 0
-        self.oil_collision = False
-        self.falling = False
-        self.check_lad = False
-        self.bottom = self.rect
-
-    def update(self):
-        if self.y_change < 8 and not self.falling:
-            barrel.y_change += 2
-        for i in range(len(plats)):
-            if self.bottom.colliderect(plats[i]):
-                self.y_change = 0
-                self.falling = False
-        # if self.rect.colliderect(oil_drum):
-
-    def check_fall(self):
-        pass
-        
-    def draw(self):
-        screen.blit(pygame.transform.rotate(barrel_img, 90 * self.pos), self.rect.topleft)
-
-class Bridge:
-    def __init__(self, x_pos, y_pos, length):
-        self.x_pos = x_pos * section_width
-        self.y_pos = y_pos 
-        self.length = length
-        self.top = self.draw()
-
-    def draw(self):
-        line_width = 7
-        platform_color = (225, 51, 129)
-        for i in range(self.length):
-            bot_coord = self.y_pos + section_height
-            left_coord = self.x_pos + (section_width * i)
-            mid_coord = left_coord + (section_width * 0.5)
-            right_coord = left_coord + section_width
-            top_coord = self.y_pos
-            # draw 4 lines, top, bot, left diag, right diag
-            pygame.draw.line(screen, platform_color, (left_coord, top_coord), (right_coord, top_coord), line_width)
-            pygame.draw.line(screen, platform_color, (left_coord, bot_coord), (right_coord, bot_coord), line_width)
-            pygame.draw.line(screen, platform_color, (left_coord, bot_coord), (mid_coord, top_coord), line_width)
-            pygame.draw.line(screen, platform_color, (mid_coord, top_coord), (right_coord, bot_coord), line_width)
-        # get the top platform 'surface'
-        top_line = pygame.rect.Rect((self.x_pos, self.y_pos), (self.length * section_width, 2))
-        # pygame.draw.rect(screen, 'blue', top_line)
-        return top_line
-
-class Ladder:
-    def __init__(self, x_pos, y_pos, length):
-        self.x_pos = x_pos * section_width
-        self.y_pos = y_pos
-        self.length = length
-        self.body = self.draw()
-
-    def draw(self):
-        line_width = 3
-        lad_color = 'light blue'
-        lad_height = 0.6
-        for i in range(self.length):
-            top_coord = self.y_pos + lad_height * section_height * i
-            bot_coord = top_coord + lad_height * section_height
-            mid_coord = (lad_height / 2) * section_height + top_coord
-            left_coord = self.x_pos
-            right_coord = left_coord + section_width
-            pygame.draw.line(screen, lad_color, (left_coord, top_coord), (left_coord, bot_coord), line_width)
-            pygame.draw.line(screen, lad_color, (right_coord, top_coord), (right_coord, bot_coord), line_width)
-            pygame.draw.line(screen, lad_color, (left_coord, mid_coord), (right_coord, mid_coord), line_width)
-        body = pygame.rect.Rect((self.x_pos, self.y_pos - section_height),
-                                (section_width, (lad_height * self.length * section_height + section_height)))
-        return body
-
-
-# function to draw platforms and ladders
-def draw_screen():
-    platforms = []
-    climbers = []
-    ladder_objs = []
-    bridge_objs = []
-
-    ladders = levels[active_level]['ladders']
-    bridges = levels[active_level]['bridges']
-
-    for ladder in ladders:
-        ladder_objs.append(Ladder(*ladder))
-        if ladder[2] >= 3:
-            climbers.append(ladder_objs[-1].body)
-
-    for bridge in bridges:
-        bridge_objs.append(Bridge(*bridge))
-        platforms.append(bridge_objs[-1].top)
-
-    return platforms, climbers
-
-barrels = pygame.sprite.Group()
-
-run = True
-while run:
-    screen.fill('black')
-    timer.tick(fps)
-    if barrel_count < barrel_spawn_time:
-        barrel_count += 1 
-    else:
-        barrel_count = random.randint(0, 120)
-        barrel_time = barrel_count - barrel_spawn_time
-        barrel = Barrel(270, 270)
-        barrels.add(barrel)
-    for barrel in barrels:
-        barrel.draw()
-        barrel.check_fall()
-    barrels.update()
-    # draw platforms and ladders on the screen in dedicated function
-    plats, lads = draw_screen()
-
+# Inicializa o Pygame e carrega os recursos necessários
+def inicializa():
+    pygame.init()  # Inicializa o Pygame
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    # Cria a janela do jogo e preenche com preto
+    window = pygame.display.set_mode(dimensoes, vsync=True, flags=pygame.SCALED)
+    window.fill((0, 0, 0))
+    pygame.display.set_caption('First game')  # Define o título da janela
 
-    pygame.display.flip()
-pygame.quit()
+    # Carrega as imagens do campo de estrelas e da nave espacial
+    assets = {}
+    assets['starfield'] = pygame.image.load('assets/img/starfield.png')
+    assets['starfield'] = pygame.transform.scale(assets['starfield'], dimensoes)
+    assets['ponte'] = pygame.image.load('assets/img/bridge.png') 
+    assets['ponte'] = pygame.transform.scale(assets['ponte'], (90, 50))
+
+
+
+    # Inicializa o estado do jogo
+    state = {
+        't0': -1,   # Tempo inicial
+    }
+
+
+
+    return window, assets, state
+    
+# Recebe eventos do Pygame
+def recebe_eventos(state, window):
+
+    # Calculo do fps
+    t1 = pygame.time.get_ticks()
+    t0 = state['t0']
+    dt = (t1 - t0) / 1000
+    fps = 1 / dt  # Calcula a taxa de quadros por segundo (FPS)
+    state['t0'] = t1
+    state['fps'] = fps
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # Se o evento QUIT foi acionado, retorna False
+            return False
+
+
+
+    return True
+
+
+
+def desenha(window, assets, state):
+    
+    window.blit(assets['starfield'], (0, 0))
+    window.blit(assets['ponte'], (0, 0))
+    
+    pygame.display.update()  # Atualiza a tela
+
+# Loop principal do jogo
+def game_loop(window, assets, state):
+    
+    while recebe_eventos(state, window):  # Continua recebendo eventos e desenhando na tela até que o usuário feche a janela do jogo
+        desenha(window, assets, state)
+
+if __name__ == '__main__':
+    
+    w, assets, state = inicializa()  # Inicializa o Pygame e carrega os recursos necessários
+    game_loop(w, assets, state)  # Inicia o loop principal do jogo
